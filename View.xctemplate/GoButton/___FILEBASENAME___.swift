a7d8815d707283550c056f22fd9b6708f4a96bd8
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-11-25 12:09:35 +0000

    new templates

---
+++ View.xctemplate/GoButton/___FILEBASENAME___.swift
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import GoFramework
+
+class ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_goSubclass___ {
+    
+    
+    // MARK: Layout
+    
+    override func layoutElements() {
+        super.layoutElements()
+    }
+    
+    // MARK: Creating elements
+    
+    override func configureElements() {
+        super.configureElements()
+    }
+    
+    // MARK: Initialization
+    
+    override func configure() {
+        super.configure()
+    }
+    
+}
